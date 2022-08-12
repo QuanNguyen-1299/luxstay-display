bb6adaa5d1640533f422de6d1d2dba5c922bda9a
CaptainQ-12 <106942422+CaptainQ-12@users.noreply.github.com>
2022-08-12 17:33:36 +0700

    upload source code

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+$(document).ready(function () {
+    $('.slide-carousel').slick({
+        infinite:false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows:true,
+        prevArrow:`<button type="button" class="slick-prev slick-arrow"></button>`,
+        nextArrow:`<button type="button" class="slick-next slick-arrow"></button>`
+    });
+});
 
-
+$(document).ready(function(){
+    $('.banner__endow-location').slick({
+      slidesToShow:3,
+      arrows:false
+    });
+  });
