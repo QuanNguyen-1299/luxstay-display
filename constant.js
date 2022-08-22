f04bf912c442b3bb14ee9750074ce59f2c739b15
CaptainQ-12 <106942422+CaptainQ-12@users.noreply.github.com>
2022-08-23 02:58:20 +0700

    upload source code

---
+++ constant.js
@@ -0,0 +1,117 @@
+const breakPoint =
+    [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                arrows: true,
+                slidesToShow: 4,
+            }
+        }
+    ]
+
+const breakPoint1 =
+    [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                arrows: false,
+                slidesToShow: 3,
+            }
+        }
+    ]
+
+const breakPoint2 =
+    [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                arrows: true,
+                slidesToShow: 3,
+            }
+        }
+    ]
+
+
+
+const myProperty = {
+    className: '.slide-carousel',
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    action: true
+}
+const myProperty1 = {
+    className: '.banner__endow-location',
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    action: false
+}
+const myProperty2 = {
+    className: '.suggestions-slider',
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    action: true
+}
+const myProperty3 = {
+    className: '.discover-slider',
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    action: true
+}
+const myProperty4 = {
+    className: '.user-manual-slider',
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    action: false
+}
--- main.js
+++ main.js
@@ -1,17 +1,41 @@
+
 $(document).ready(function () {
-    $('.slide-carousel').slick({
-        infinite:false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        arrows:true,
-        prevArrow:`<button type="button" class="slick-prev slick-arrow"></button>`,
-        nextArrow:`<button type="button" class="slick-next slick-arrow"></button>`
-    });
+  //slide-carousel
+  slickSlide(myProperty, breakPoint);
+  //banner__endow-location
+  slickSlide(myProperty1, breakPoint1)
+  //suggestions-location
+  slickSlide(myProperty2, breakPoint)
+  //discover-location
+  slickSlide(myProperty3, breakPoint2)
+  //user-manual-location
+  slickSlide(myProperty4, breakPoint)
+
 });
 
-$(document).ready(function(){
-    $('.banner__endow-location').slick({
-      slidesToShow:3,
-      arrows:false
-    });
+
+
+
+const slickSlide = (property, responsive) => {
+  $(property.className).slick({
+    infinite: property.infinite,
+    slidesToShow: property.slidesToShow,
+    slidesToScroll: property.slidesToScroll,
+    arrows: property.arrows,
+    prevArrow: property.action && `<button type="button" class="slick-prev slick-arrow arrow-sugges "><i class="fa-solid fa-angle-left"></i></button>`,
+    nextArrow: property.action && `<button type="button" class="slick-next slick-arrow arrow-sugges "><i class="fa-solid fa-angle-right"></i></button>`,
+    responsive: responsive
+
   });
+}
+
+function openNav() {
+  document.getElementById("menu-mb").style.transform = `translateX(0%)`
+  const blockOverlay = document.getElementsByClassName("nav-overlay")
+  blockOverlay.setAttribute('class','d-block')
+}
+
+function closeNav() {
+  document.getElementById("menu-mb").style.transform = `translateX(-100%)`
+
+}
